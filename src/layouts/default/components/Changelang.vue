1c3e24983678c574d1e82d3d88d6430ea2c6adc0
Ferhat ER <lersmooth@gmail.com>
2023-02-28 22:32:58 +0300

    nav

--- src/layouts/default/components/Changelang.vue
+++ src/layouts/default/components/Changelang.vue
@@ -1,20 +1,20 @@
 <template>
   <v-menu>
     <template v-slot:activator="{ props }">
-      <v-btn icon="mdi-translate" v-bind="props"></v-btn>
+      <v-btn icon="mdi-translate" v-bind="props"> </v-btn>
     </template>
     <v-list density="compact">
       <v-list-item
-      dense
+        dense
         class="cursor-pointer"
         v-for="locale in lang.availableLocales"
         :key="`locale-${locale}`"
         @click="changeLocale(locale)"
       >
-    <v-avatar size="28">
-      <img :src="`src/assets/svg/flags/${locale}.svg`" :alt="locale">
-    </v-avatar>
-    </v-list-item>
+        <v-avatar size="28">
+          <img :src="`src/assets/svg/flags/${locale}.svg`" :alt="locale" />
+        </v-avatar>
+      </v-list-item>
     </v-list>
   </v-menu>
 </template>
@@ -26,15 +26,15 @@ const lang = useI18n();
 const setLang = () => {
   const currentLang = localStorage.getItem("currentLang");
   if (currentLang == null) {
-    localStorage.setItem('currentLang', lang.locale.value);
-  }else{
+    localStorage.setItem("currentLang", lang.locale.value);
+  } else {
     lang.locale.value = currentLang;
   }
 };
 
 const changeLocale = (locale) => {
   lang.locale.value = locale;
-  localStorage.setItem('currentLang', locale)
+  localStorage.setItem("currentLang", locale);
 };
 
 onMounted(() => {
